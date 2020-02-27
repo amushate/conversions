a23eb6ec3d57a5c52a2cf9a80b5d0fa29f831ebf
Anesu Mushate <anesumushate@gmail.com>
2020-02-27 20:22:08 +0200

    project structure setup

---
+++ src/test/java/za/co/playsafe/conversions/ConversionsApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.playsafe.conversions;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
