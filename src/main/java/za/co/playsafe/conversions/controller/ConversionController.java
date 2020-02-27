31643126708789c73427e3d523b4464165a0c30b
Anesu Mushate <anesumushate@gmail.com>
2020-02-27 21:05:40 +0200

    Temperature conversion endpoinds

---
+++ src/main/java/za/co/playsafe/conversions/controller/ConversionController.java
@@ -0,0 +1,44 @@
+package za.co.playsafe.conversions.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import za.co.playsafe.conversions.dto.ConversionRequestDto;
+import za.co.playsafe.conversions.service.ConversionService;
+
+@RestController
+@RequestMapping("/conversions")
+public class ConversionController {
+
+	@Autowired
+	private ConversionService conversionService;
+
+	
+	@PostMapping("/ktoc")
+	public double kelvinToCelsius(@Valid @RequestBody ConversionRequestDto request){
+		return conversionService.kelvinTocelsius(request.getInputValue());
+	}
+
+	
+	@PostMapping("/ctok")
+	public double celsiusToKelvin(@Valid @RequestBody ConversionRequestDto request){
+		return conversionService.celsiusToKelvin(request.getInputValue());
+	}
+	
+	
+	@PostMapping("/mtok")
+	public double milesToKilometers(@Valid @RequestBody ConversionRequestDto request){
+		return conversionService.milesToKilometers(request.getInputValue());
+	}
+	
+	
+	@PostMapping("/ktom")
+	public double kilometersToMiles(@Valid @RequestBody ConversionRequestDto request){
+		return conversionService.kilometersToMiles(request.getInputValue());
+	}
+}
